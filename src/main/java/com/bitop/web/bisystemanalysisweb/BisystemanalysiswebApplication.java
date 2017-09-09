12ed04a9677d56ea5e77e73678acde245c82abe7
mustang2247 <mustang2247@gmail.com>
2017-09-09 21:51:30 +0800

    init import

---
+++ src/main/java/com/bitop/web/bisystemanalysisweb/BisystemanalysiswebApplication.java
@@ -0,0 +1,12 @@
+package com.bitop.web.bisystemanalysisweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BisystemanalysiswebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BisystemanalysiswebApplication.class, args);
+	}
+}
---
+++ src/test/java/com/bitop/web/bisystemanalysisweb/BisystemanalysiswebApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bitop.web.bisystemanalysisweb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BisystemanalysiswebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
