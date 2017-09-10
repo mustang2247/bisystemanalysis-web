83e63675861b51a2b263c59d5a9121a122a055d9
mustang2247 <mustang2247@gmail.com>
2017-09-10 20:14:50 +0800

    init import code

--- src/main/java/com/bitop/web/bisystemanalysisweb/model/user/UserController.java
+++ src/main/java/com/bitop/web/bisystemanalysisweb/model/user/UserController.java
@@ -1,4 +1,4 @@
-package com.bitop.web.bisystemanalysisweb.model.controller;
+package com.bitop.web.bisystemanalysisweb.model.user;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.Locale;
 
 @Controller
-@RequestMapping("/")
-public class IndexController {
-    @RequestMapping({"/","/index"})
+@RequestMapping("/user/")
+public class UserController {
+    @RequestMapping({""})
     public String index(Locale locale, Model model){
         return "index";
     }
