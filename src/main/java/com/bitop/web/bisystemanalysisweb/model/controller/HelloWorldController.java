20cc9c898d83272aeef7f8b189b6257dd6fcb739
mustangkong <mustang2247@163.com>
2017-09-12 19:18:39 +0800

    add restfull api

--- src/main/java/com/bitop/web/bisystemanalysisweb/model/controller/HelloWorldController.java
+++ src/main/java/com/bitop/web/bisystemanalysisweb/model/controller/HelloWorldController.java
@@ -1,13 +1,20 @@
 package com.bitop.web.bisystemanalysisweb.model.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/track")
 public class HelloWorldController {
 	
-    @RequestMapping("/hello")
+    @RequestMapping("")
     public String index() {
         return "hello welcome";
     }
+
+    @RequestMapping(value = "/install/{data}", method = {RequestMethod.POST, RequestMethod.GET})
+    @ResponseBody
+    public String install(@PathVariable("data") String data){
+        return data;
+    }
+
 }
